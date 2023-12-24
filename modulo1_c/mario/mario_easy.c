55e99ef32c7b524aafd037e9eb66e69c83c8e41a
Nanda <124320003+GirlSM@users.noreply.github.com>
2023-12-24 17:45:22 -0300

    Create mario_easy.c

---
+++ modulo1_c/mario/mario_easy.c
@@ -0,0 +1,31 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Recebe uma altura do usuário entre 1 e 8;
+    int h;
+    do
+    {
+        h = get_int("Altura: ");
+    }
+    while (h < 1 || h > 8);
+
+    // acrescenta uma quebra de linha até que atinja a altura
+    for (int i = 0; i <= h; i++)
+    {
+        // acrescenta uma # de acordo com a quantidade de quebra de linha
+        for (int b = 0; b < i; b++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+
+        // insere os * de acordo com a quantidade de # em cada linha
+        for (int j = h - 1; j > i; j--)
+        {
+            printf(" ");
+        }
+    }
+}
